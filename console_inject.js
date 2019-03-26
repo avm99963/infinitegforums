847ee633cc23f80befa177a020aa6c4da8a1ceda
avm99963 <jocdeladria@gmail.com>
2019-03-27 00:57:44 +0100

    Version 0.0.1

---
+++ console_inject.js
@@ -0,0 +1,36 @@
+function mutationCallback(mutationList, observer) {
+  mutationList.forEach((mutation) => {
+    if (mutation.type == "childList") {
+      mutation.addedNodes.forEach(function (node) {
+		    if ((typeof node.classList !== "undefined") && node.classList.contains("view-more-button-container")) {
+          intersectionObserver.observe(node.querySelector(".view-more-button"));
+        }
+      });
+    }
+  });
+}
+
+function intersectionCallback(entries, observer) { 
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.click();
+    }
+  });
+};
+
+var observerOptions = {
+  childList: true,
+  attributes: true,
+  subtree: true
+}
+
+var mutationObserver = new MutationObserver(mutationCallback);
+mutationObserver.observe(document.querySelector(".scrollable-content"), observerOptions);
+
+var intersectionOptions = {
+  root: document.querySelector('.scrollable-content'),
+  rootMargin: '0px',
+  threshold: 1.0
+}
+
+var intersectionObserver = new IntersectionObserver(intersectionCallback, intersectionOptions);
---
+++ tailwind_inject.js
@@ -0,0 +1,15 @@
+function intersectionCallback(entries, observer) {
+  console.log(entries);
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.click();
+    }
+  });
+};
+
+var intersectionOptions = {
+  threshold: 1.0
+}
+
+var intersectionObserver = new IntersectionObserver(intersectionCallback, intersectionOptions);
+intersectionObserver.observe(document.querySelector(".thread-list-threads__load-more-button"));
