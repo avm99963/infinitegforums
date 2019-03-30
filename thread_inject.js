134f1ef4c4b218f7b8dc8483e664450d2c9d9185
avm99963 <jocdeladria@gmail.com>
2019-03-30 19:05:27 +0100

    Fixes #3

--- thread_inject.js
+++ thread_inject.js
@@ -13,9 +13,14 @@ var intersectionOptions = {
 }
 
 chrome.storage.sync.get(null, function(items) {
-  var button = document.querySelector(".thread-all-replies__load-more-button");
-  if (items.thread && button !== null) {
-    intersectionObserver = new IntersectionObserver(intersectionCallback, intersectionOptions);
-    intersectionObserver.observe(button);
+  var redirectLink = document.querySelector(".thread-question__open-in-community-console-button");
+  if (items.redirect && redirectLink !== null) {
+    window.location = redirectLink.href;
+  } else {
+    var button = document.querySelector(".thread-all-replies__load-more-button");
+    if (items.thread && button !== null) {
+      intersectionObserver = new IntersectionObserver(intersectionCallback, intersectionOptions);
+      intersectionObserver.observe(button);
+    }
   }
 });
