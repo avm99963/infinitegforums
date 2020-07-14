6c7920fff2fbbd62089ad2faa46e7752a3a90a0f
avm99963 <me@avm99963.com>
2020-07-14 18:18:20 +0200

    Replaces batchduplicate implementation w/ CC flag
    The CC now implements batch duplicate functionality, hidden behind the
    enableBulkDuplication flag. This commit removes our basic implementation
    of that feature and instead force-enables that CC flag when the batch
    duplication option is enabled.
    
    Change-Id: Ic2ac504026073087a767c2d264307acb278878da

--- src/console_inject_start.js
+++ src/console_inject_start.js
@@ -1,9 +1,13 @@
 chrome.storage.sync.get(null, function(items) {
-  if (items.loaddrafts || items.escalatethreads || items.movethreads) {
+  if (items.loaddrafts || items.batchduplicate) {
     var startup = JSON.parse(document.querySelector("html").getAttribute("data-startup"));
 
     if (items.loaddrafts) {
       startup[4][13] = true;
+    }
+
+    if (items.batchduplicate) {
+      startup[4][58] = true;
     }
 
     document.querySelector("html").setAttribute("data-startup", JSON.stringify(startup));
