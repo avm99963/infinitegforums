avm99963/infinitegforums

commit adf90864c7aded366ea02bb7d0242747015b3ec5

Deprecated obsolete options which are now part of the vanilla CC/TW Forums experience and fixed a bug caused to an internal TW HTML structure change

src/thread_inject.js
@@ -13,7 +13,7 @@ var intersectionOptions = {
 }
 
 chrome.storage.sync.get(null, function(items) {
-  var redirectLink = document.querySelector(".thread-question__open-in-community-console-button");
+  var redirectLink = document.querySelector(".community-console");
   if (items.redirect && redirectLink !== null) {
     window.location = redirectLink.href;
   } else {

src/background.js
@@ -2,7 +2,7 @@ function isEmpty(obj) {
   return Object.keys(obj).length === 0;
 }
 
-var defaultOptions = {
+const defaultOptions = {
   "list": true,
   "thread": true,
   "threadall": false,

src/console_inject.js
@@ -46,10 +46,6 @@ function mutationCallback(mutationList, observer) {
     if (mutation.type == "childList") {
       mutation.addedNodes.forEach(function (node) {
         if (typeof node.classList !== "undefined") {
-          if (options.list && node.classList.contains("view-more-button-container")) {
-            intersectionObserver.observe(node.querySelector(".view-more-button"));
-          }
-
           if (options.thread && node.classList.contains("load-more-bar")) {
             intersectionObserver.observe(node.querySelector(".load-more-button"));
           }
@@ -105,7 +101,7 @@ function mutationCallback(mutationList, observer) {
   });
 }
 
-function intersectionCallback(entries, observer) { 
+function intersectionCallback(entries, observer) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.click();

src/console_inject_start.js
@@ -6,14 +6,6 @@ chrome.storage.sync.get(null, function(items) {
       startup[4][13] = true;
     }
 
-    if (items.escalatethreads) {
-      startup[4][20] = true;
-    }
-
-    if (items.movethreads) {
-      startup[4][18] = true;
-    }
-
     document.querySelector("html").setAttribute("data-startup", JSON.stringify(startup));
   }
 });

src/options.html
@@ -7,7 +7,6 @@
   </head>
   <body>
     <p>
-      <input type="checkbox" id="list"> <label for="list" data-i18n="list"></label><br>
       <input type="checkbox" id="thread"> <label for="thread" data-i18n="thread"></label><br>
       <input type="checkbox" id="threadall"> <label for="threadall" data-i18n="threadall"></label>
     </p>
@@ -18,8 +17,6 @@
       <input type="checkbox" id="history"> <label for="history" data-i18n="history"></label><br>
       <input type="checkbox" id="loaddrafts"> <label for="loaddrafts" data-i18n="loaddrafts"></label> <span style="color: gray;" data-i18n="experimental_label"></span><br>
       <input type="checkbox" id="batchduplicate"> <label for="batchduplicate" data-i18n="batchduplicate"></label> <span style="color: gray;" data-i18n="experimental_label"></span><br>
-      <input type="checkbox" id="escalatethreads"> <label for="escalatethreads" data-i18n="escalatethreads"></label> <span style="color: gray;" data-i18n="experimental_label"></span><br>
-      <input type="checkbox" id="movethreads"> <label for="movethreads" data-i18n="movethreads"></label> <span style="color: gray;" data-i18n="experimental_label"></span><br>
     </p>
     <p style="text-align: center;"><button id="save" data-i18n="save"></button></p>
     <script src="options.js"></script>

src/options.js
@@ -2,7 +2,7 @@ function isEmpty(obj) {
   return Object.keys(obj).length === 0;
 }
 
-var defaultOptions = {
+const defaultOptions = {
   "list": true,
   "thread": true,
   "threadall": false,
@@ -14,6 +14,12 @@ var defaultOptions = {
   "escalatethreads": false,
   "movethreads": false
 };
+
+const deprecatedOptions = [
+  "list",
+  "escalatethreads",
+  "movethreads"
+];
 
 function cleanUpOptions(options) {
   var ok = true;
@@ -35,7 +41,8 @@ function save() {
   var options = defaultOptions;
 
   Object.keys(options).forEach(function (opt) {
-    options[opt] = document.querySelector("#"+opt).checked;
+    if (deprecatedOptions.includes(opt)) return;
+    options[opt] = document.querySelector("#"+opt).checked || false;
   });
 
   chrome.storage.sync.set(options, function() {
@@ -60,7 +67,7 @@ window.addEventListener("load", function() {
     items = cleanUpOptions(items);
 
     Object.keys(defaultOptions).forEach(function(opt) {
-      if (items[opt] === true) {
+      if (items[opt] === true && !deprecatedOptions.includes(opt)) {
         document.querySelector("#"+opt).checked = true;
       }
     });

commit 7db655fea4ed2d6b4f65ea4527bb55e61e760882

Fix: don't redirect TW new thread page to the CC
As pointed out at pekb/thread/60972469?msgid=6546485, the new thread
page is being incorrectly redirected to the CC. This commit fixes this.

This bug is a regression introduced by commit adf9086.

Change-Id: I9d858fc4e6822662bc3aca6d1925b7c6bf42dab7

src/thread_inject.js
@@ -13,18 +13,29 @@ var intersectionOptions = {
 }
 
 chrome.storage.sync.get(null, function(items) {
+  var path = document.location.pathname.split("/");
+  if (path[path.length - 1] == "new" ||
+      (path.length > 1 && path[path.length - 1] == "" &&
+          path[path.length - 2] == "new")) {
+    return;
+  }
+
   var redirectLink = document.querySelector(".community-console");
   if (items.redirect && redirectLink !== null) {
     window.location = redirectLink.href;
   } else {
-    var button = document.querySelector(".thread-all-replies__load-more-button");
+    var button =
+        document.querySelector(".thread-all-replies__load-more-button");
     if (items.thread && button !== null) {
-      intersectionObserver = new IntersectionObserver(intersectionCallback, intersectionOptions);
+      intersectionObserver =
+          new IntersectionObserver(intersectionCallback, intersectionOptions);
       intersectionObserver.observe(button);
     }
-    var allbutton = document.querySelector(".thread-all-replies__load-all-button");
+    var allbutton =
+        document.querySelector(".thread-all-replies__load-all-button");
     if (items.threadall && button !== null) {
-      intersectionObserver = new IntersectionObserver(intersectionCallback, intersectionOptions);
+      intersectionObserver =
+          new IntersectionObserver(intersectionCallback, intersectionOptions);
       intersectionObserver.observe(allbutton);
     }
   }