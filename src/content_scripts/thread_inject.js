36b8dbcad8bebe8eaed6fb2bfbcd7c3d3d3164cc
avm99963 <me@avm99963.com>
2020-09-01 21:16:19 +0200

    Exclude matches with manifest.json instead of JS
    Some wrong matches were being excluded with Javascript code testing the
    location.href value. This commit deletes these Javascript tests and
    implements them directly in the manifest.gjson file by placing them
    inside the exclude_matches argument.
    
    Change-Id: Idba4086b7061eaea837d7c7a3e3564c144535e42

--- src/content_scripts/thread_inject.js
+++ src/content_scripts/thread_inject.js
@@ -1,45 +1,35 @@
-var CCRegex = /^https:\/\/support\.google\.com\/s\/community/;
-if (!CCRegex.test(location.href)) {
-  var intersectionObserver;
+var intersectionObserver;
 
-  function intersectionCallback(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.click();
-      }
-    });
-  };
-
-  var intersectionOptions = {
-    threshold: 1.0,
-  };
-
-  chrome.storage.sync.get(null, function(items) {
-    var path = document.location.pathname.split('/');
-    if (path[path.length - 1] == 'new' ||
-        (path.length > 1 && path[path.length - 1] == '' &&
-         path[path.length - 2] == 'new')) {
-      return;
-    }
-
-    var redirectLink = document.querySelector('.community-console');
-    if (items.redirect && redirectLink !== null) {
-      window.location = redirectLink.href;
-    } else {
-      var button =
-          document.querySelector('.thread-all-replies__load-more-button');
-      if (items.thread && button !== null) {
-        intersectionObserver =
-            new IntersectionObserver(intersectionCallback, intersectionOptions);
-        intersectionObserver.observe(button);
-      }
-      var allbutton =
-          document.querySelector('.thread-all-replies__load-all-button');
-      if (items.threadall && button !== null) {
-        intersectionObserver =
-            new IntersectionObserver(intersectionCallback, intersectionOptions);
-        intersectionObserver.observe(allbutton);
-      }
+function intersectionCallback(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.click();
     }
   });
-}
+};
+
+var intersectionOptions = {
+  threshold: 1.0,
+};
+
+chrome.storage.sync.get(null, function(items) {
+  var redirectLink = document.querySelector('.community-console');
+  if (items.redirect && redirectLink !== null) {
+    window.location = redirectLink.href;
+  } else {
+    var button =
+        document.querySelector('.thread-all-replies__load-more-button');
+    if (items.thread && button !== null) {
+      intersectionObserver =
+          new IntersectionObserver(intersectionCallback, intersectionOptions);
+      intersectionObserver.observe(button);
+    }
+    var allbutton =
+        document.querySelector('.thread-all-replies__load-all-button');
+    if (items.threadall && button !== null) {
+      intersectionObserver =
+          new IntersectionObserver(intersectionCallback, intersectionOptions);
+      intersectionObserver.observe(allbutton);
+    }
+  }
+});
