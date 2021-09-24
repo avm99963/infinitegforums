2092c5b1c5bb8b81f318145f79c37814d32a4b19
Adrià Vilanova Martínez <me@avm99963.com>
2021-09-24 23:40:31 +0200

    Fix: "ask" link in thread lists w/ infinite scroll
    In thread lists, the "ask" link in thread lists appears after the "load
    more" button. If infinite scroll is enabled, this means it can't be
    clicked. This change fixes this by moving the "ask community" card
    before the "load more" button.
    
    Fixed: twpowertools:73
    Change-Id: I8c331ba463148110ef8f63d1fcb6e934fa3983b4

--- src/contentScripts/publicForum.js
+++ src/contentScripts/publicForum.js
@@ -20,5 +20,16 @@ getOptions('list').then(options => {
     intersectionObserver =
         new IntersectionObserver(intersectionCallback, intersectionOptions);
     intersectionObserver.observe(button);
+
+    // On a best-effort basis, move the "ask community" card before the "load
+    // more" button, if both exist.
+    let buttonContainer = document.querySelector(
+        '.thread-list__threads > .thread-list-threads__load-more-container');
+    let askCard =
+        document.querySelector('.thread-list__threads > .ask-community-card');
+    if (buttonContainer !== null && askCard !== null) {
+      let threadList = document.querySelector('.thread-list__threads');
+      threadList.insertBefore(askCard, buttonContainer);
+    }
   }
 });
