7ef7943e62c6d35241c6b37e5378acc57f6fce5c
Adrià Vilanova Martínez <me@avm99963.com>
2025-04-12 20:36:11 +0200

    refactor(batch-lock): convert core code to typescript
    Change-Id: I6a6a636ca04dac711e8156a9eb8cd27c8f845cd5

--- src/features/batchLock/core/batchLock.ts
+++ src/features/batchLock/core/batchLock.ts
@@ -1,54 +1,62 @@
-import {isOptionEnabled} from '../../../common/options/optionsUtils.js';
-import {addButtonToThreadListActions, shouldAddBtnToActionBar, softRefreshView} from '../../../contentScripts/communityConsole/utils/common.js';
+import { isOptionEnabled } from '../../../common/options/optionsUtils.js';
+import {
+  addButtonToThreadListActions,
+  shouldAddBtnToActionBar,
+  softRefreshView,
+} from '../../../contentScripts/communityConsole/utils/common.js';
 
 const kLockDebugId = 'twpt-lock';
 export const kModalPaneSelector = '.pane[pane-id="default--1"]';
 
-export var batchLock = {
-  shouldAddButton(node) {
+export const batchLock = {
+  shouldAddButton(node: Node) {
     return shouldAddBtnToActionBar(kLockDebugId, node);
   },
   createDialog() {
-    var modal = document.querySelector(kModalPaneSelector);
+    const modal = document.querySelector(kModalPaneSelector);
 
-    var dialog = document.createElement('material-dialog');
+    if (!(modal instanceof HTMLElement)) {
+      throw new Error('Modal is not an HTMLElement.');
+    }
+
+    const dialog = document.createElement('material-dialog');
     dialog.setAttribute('role', 'dialog');
     dialog.setAttribute('aria-modal', 'true');
     dialog.classList.add('TWPT-dialog');
 
-    var header = document.createElement('header');
+    const header = document.createElement('header');
     header.setAttribute('role', 'presentation');
     header.classList.add('TWPT-dialog-header');
 
-    var title = document.createElement('div');
+    const title = document.createElement('div');
     title.classList.add('TWPT-dialog-header--title', 'title');
     title.textContent = chrome.i18n.getMessage('inject_lockbtn');
 
     header.append(title);
 
-    var main = document.createElement('main');
+    const main = document.createElement('main');
     main.setAttribute('role', 'presentation');
     main.classList.add('TWPT-dialog-main');
 
-    var p = document.createElement('p');
+    const p = document.createElement('p');
     p.textContent = chrome.i18n.getMessage('inject_lockdialog_desc');
 
     main.append(p);
 
     dialog.append(header, main);
 
-    var footers = [['lock', 'unlock', 'cancel'], ['close']];
+    const footers = [['lock', 'unlock', 'cancel'], ['close']];
 
-    for (var i = 0; i < footers.length; ++i) {
-      var footer = document.createElement('footer');
+    for (let i = 0; i < footers.length; ++i) {
+      const footer = document.createElement('footer');
       footer.setAttribute('role', 'presentation');
       footer.classList.add('TWPT-dialog-footer');
-      footer.setAttribute('data-footer-id', i);
+      footer.setAttribute('data-footer-id', i.toFixed(0));
 
       if (i > 0) footer.classList.add('is-hidden');
 
-      footers[i].forEach(action => {
-        var btn = document.createElement('material-button');
+      footers[i].forEach((action) => {
+        const btn = document.createElement('material-button');
         btn.setAttribute('role', 'button');
         btn.classList.add('TWPT-dialog-footer-btn');
         if (i == 1) btn.classList.add('is-disabled');
@@ -56,9 +64,9 @@ export var batchLock = {
         switch (action) {
           case 'lock':
           case 'unlock':
-            btn.addEventListener('click', _ => {
+            btn.addEventListener('click', (_) => {
               if (btn.classList.contains('is-disabled')) return;
-              var message = {
+              const message = {
                 action,
                 prefix: 'TWPT-batchlock',
               };
@@ -68,7 +76,7 @@ export var batchLock = {
 
           case 'cancel':
           case 'close':
-            btn.addEventListener('click', _ => {
+            btn.addEventListener('click', (_) => {
               if (btn.classList.contains('is-disabled')) return;
 
               if (action == 'close') softRefreshView();
@@ -80,16 +88,17 @@ export var batchLock = {
             break;
         }
 
-        var content = document.createElement('div');
+        const content = document.createElement('div');
         content.classList.add('content', 'TWPT-dialog-footer-btn--content');
-        content.textContent =
-            chrome.i18n.getMessage('inject_lockdialog_btn_' + action);
+        content.textContent = chrome.i18n.getMessage(
+          'inject_lockdialog_btn_' + action,
+        );
 
         btn.append(content);
         footer.append(btn);
       });
 
-      var clear = document.createElement('div');
+      const clear = document.createElement('div');
       clear.style.clear = 'both';
 
       footer.append(clear);
@@ -101,12 +110,16 @@ export var batchLock = {
     modal.classList.add('visible', 'modal');
     modal.style.display = 'flex';
   },
-  addButtonIfEnabled(readToggle) {
-    isOptionEnabled('batchlock').then(isEnabled => {
+  addButtonIfEnabled(readToggle: Node) {
+    isOptionEnabled('batchlock').then((isEnabled) => {
       if (isEnabled) {
         let tooltip = chrome.i18n.getMessage('inject_lockbtn');
         let btn = addButtonToThreadListActions(
-            readToggle, 'lock', kLockDebugId, tooltip);
+          readToggle,
+          'lock',
+          kLockDebugId,
+          tooltip,
+        );
         btn.addEventListener('click', () => {
           this.createDialog();
         });
@@ -115,7 +128,7 @@ export var batchLock = {
   },
 };
 
-function removeChildNodes(node) {
+function removeChildNodes(node: Node) {
   while (node?.firstChild) {
     node.removeChild(node.firstChild);
   }
