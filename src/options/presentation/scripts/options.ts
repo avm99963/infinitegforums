4f4e3ce685e286d1cb356242787b05db44f4d11d
Adrià Vilanova Martínez <me@avm99963.com>
2025-08-23 22:15:26 +0200

    feat(options): enable new options page for anglophone users
    The new options page will be dogfooded with English-speaking users.
    While the dogfood is underway, localization will be made to other
    languages.
    
    After the translation of the new options page to a new language is
    complete, this language will be added to the supported languages list.
    Before this, users for these languages will see the old options page.
    
    Bug: twpowertools:63
    Change-Id: I6a6a6964537191ead2d696025072afa2a010a9f4

--- src/options/presentation/scripts/options.ts
+++ src/options/presentation/scripts/options.ts
@@ -4,10 +4,37 @@ import { featureCategories } from '../featureCategories';
 import '../components/App';
 import { isProdVersion } from '../../../common/extUtils';
 
-const container = document.getElementById('container');
-const app = document.createElement('options-app');
-app.optionsProvider = new OptionsProviderAdapter();
-app.optionsModifier = new OptionsModifierAdapter();
-app.featureCategories = featureCategories;
-app.isProdVersion = isProdVersion();
-container.append(app);
+const SUPPORTED_LANGUAGES: string[] = ['en'];
+
+function main() {
+  if (shouldShowOldPage()) {
+    window.location.href = chrome.runtime.getURL('options/options_old.html');
+    return;
+  }
+
+  const container = document.getElementById('container');
+  const app = document.createElement('options-app');
+  app.optionsProvider = new OptionsProviderAdapter();
+  app.optionsModifier = new OptionsModifierAdapter();
+  app.featureCategories = featureCategories;
+  app.isProdVersion = isProdVersion();
+  container.append(app);
+}
+
+function shouldShowOldPage() {
+  return !isLanguageSupported() && !isNewOptionsPageForced();
+}
+
+function isLanguageSupported() {
+  const language = chrome.i18n.getUILanguage().toLowerCase();
+  return SUPPORTED_LANGUAGES.some((supportedLanguage) =>
+    language.startsWith(supportedLanguage),
+  );
+}
+
+function isNewOptionsPageForced() {
+  const searchParams = new URLSearchParams(window.location.search);
+  return searchParams.get('no_redirect') === 'true';
+}
+
+main();
