avm99963/infinitegforums

commit 82229aef17df05becc275796d5a97497a9747805

feat(options): expose information about kill switches
This CL changes OptionsConfiguration so information about whether a kill
switch is active can be retrieved. It also modifies OptionsProvider so
it builds the new OptionsConfiguration instances with this information,
and to clarify which methods take into account kill switches and which
ones do not.

Finally, this CL raises the Typescript |target|, since we're using a new
Javascript feature (Object.hasOwn). Instead of just raising the
individual |lib| option, we raise |target| since the minimum compatible
Chromium and Firefox versions should already support all needed es2022
features.

Fixed: twpowertools:247
Change-Id: I6a6a6964084d8248dd1989f921324321838006ef

src/common/options/OptionsConfiguration.ts
@@ -1,32 +1,87 @@
+import { KillSwitchType } from './Option';
 import {
   OptionCodename,
   OptionsValues,
   optionCodenames,
+  optionsMap,
 } from './optionsPrototype';
+
+export interface OptionStatus<O extends OptionCodename> {
+  value: OptionsValues[O];
+  /**
+   * Whether the option has its kill switch enabled.
+   */
+  isKillSwitchEnabled: boolean;
+}
+
+export type OptionsStatus = {
+  [O in OptionCodename]: OptionStatus<O>;
+};
 
 /**
  * Representation of a specific configuration of the option values.
  */
 export class OptionsConfiguration {
-  constructor(public optionsValues: OptionsValues) {}
+  constructor(public optionsStatus: OptionsStatus) {}
 
+  /**
+   * Get the option value taking into account any enabled kill switches.
+   */
   getOptionValue<O extends OptionCodename>(option: O): OptionsValues[O] {
-    return this.optionsValues[option];
+    if (this.isKillSwitchEnabled(option)) {
+      return false as OptionsValues[O];
+    }
+    return this.getUserConfiguredOptionValue(option);
   }
 
+  /**
+   * Returns whether a kill switch is enabled for this option (that is, whether
+   * the option has been remotely disabled).
+   */
+  isKillSwitchEnabled(option: OptionCodename): boolean {
+    // Options which are set to ignore kill switches should never have a kill
+    // switch active.
+    if (optionsMap.get(option).killSwitchType === KillSwitchType.Ignore) {
+      return false;
+    }
+
+    return this.optionsStatus[option].isKillSwitchEnabled;
+  }
+
+  /**
+   * Get the option value as configured by the user, **without** taking into
+   * account any kill switches.
+   */
+  getUserConfiguredOptionValue<O extends OptionCodename>(
+    option: O,
+  ): OptionsValues[O] {
+    return this.optionsStatus[option].value;
+  }
+
+  /**
+   * Returns whether a feature option is enabled, taking into account any
+   * enabled kill switch.
+   *
+   * NOTE: the only admitted options are feature options (those marked with
+   * KillSwitchType.Option).
+   */
   isEnabled(option: OptionCodename): boolean {
-    const value = this.getOptionValue(option);
-    return value === true;
+    if (optionsMap.get(option).killSwitchType !== KillSwitchType.Option) {
+      return false;
+    }
+
+    return (
+      this.getOptionValue(option) === true && !this.isKillSwitchEnabled(option)
+    );
   }
 
   isEqualTo(otherConfiguration: OptionsConfiguration) {
-    for (const option of optionCodenames) {
-      const thisValue = this.getOptionValue(option);
-      const otherValue = otherConfiguration.getOptionValue(option);
-      if (thisValue !== otherValue) {
-        return false;
-      }
-    }
-    return true;
+    return optionCodenames.every(
+      (option) =>
+        this.getUserConfiguredOptionValue(option) ===
+          otherConfiguration.getUserConfiguredOptionValue(option) &&
+        this.isKillSwitchEnabled(option) ===
+          otherConfiguration.isKillSwitchEnabled(option),
+    );
   }
 }

src/common/options/optionsUtils.js
@@ -28,21 +28,24 @@ export function cleanUpOptions(options, dryRun = false) {
   return options;
 }
 
-// Returns a promise which returns the values of options |options| which are
-// stored in the sync storage area.
-//
-// |requireOptionalPermissions| will determine whether to check if the required
-// optional permissions have been granted or not to the options which have such
-// requirements. If it is true, features with missing permissions will have
-// their value set to false.
-//
-// When a kill switch is active, affected options always have their value set to
-// false.
-
 // #!if !production
 let timerId = 0;
 let randomId = btoa(Math.random().toString()).substr(10, 5);
 // #!endif
+/**
+ * Returns a promise which returns the values of options |options| which are
+ * stored in the sync storage area.
+ *
+ * |requireOptionalPermissions| will determine whether to check if the required
+ * optional permissions have been granted or not to the options which have such
+ * requirements. If it is true, features with missing permissions will have
+ * their value set to false.
+ *
+ * When a kill switch is active, affected options always have their value set to
+ * false.
+ *
+ * @deprecated Use OptionsProviderPort.
+ */
 export function getOptions(options, requireOptionalPermissions = true) {
   // #!if !production
   const timeLabel = 'getOptions--' + randomId + '-' + (timerId++);

src/infrastructure/services/options/OptionsProvider.adapter.ts
@@ -1,11 +1,15 @@
 import { Mutex, MutexInterface, withTimeout } from 'async-mutex';
 
-import { getOptions } from '../../../common/options/optionsUtils';
 import {
   OptionCodename,
+  optionCodenames,
   OptionsValues,
 } from '../../../common/options/optionsPrototype';
-import { OptionsConfiguration } from '../../../common/options/OptionsConfiguration';
+import {
+  OptionsConfiguration,
+  OptionsStatus,
+  OptionStatus,
+} from '../../../common/options/OptionsConfiguration';
 import {
   OptionsChangeListener,
   OptionsProviderPort,
@@ -47,7 +51,12 @@ export default class OptionsProviderAdapter implements OptionsProviderPort {
   }
 
   async getOptionsValues(): Promise<OptionsValues> {
-    return (await this.getOptionsConfiguration()).optionsValues;
+    const optionsConfiguration = await this.getOptionsConfiguration();
+    const entries = optionCodenames.map((codename) => [
+      codename,
+      optionsConfiguration.getOptionValue(codename),
+    ]);
+    return Object.fromEntries(entries) as OptionsValues;
   }
 
   addListener(listener: OptionsChangeListener) {
@@ -88,10 +97,40 @@ export default class OptionsProviderAdapter implements OptionsProviderPort {
 
   private async nonSafeUpdateValues() {
     const previousConfiguration = this.optionsConfiguration;
-    const currentOptionsValues = await getOptions(null);
-    this.optionsConfiguration = new OptionsConfiguration(currentOptionsValues);
-
+    this.optionsConfiguration = await this.retrieveOptionsConfiguration();
     this.notifyListenersIfApplicable(previousConfiguration);
+  }
+
+  private async retrieveOptionsConfiguration() {
+    const options = await chrome.storage.sync.get();
+    const entries = optionCodenames.map(
+      <O extends OptionCodename>(codename: O): [O, OptionStatus<O>] => [
+        codename,
+        {
+          value: options[codename] as OptionsValues[O],
+          isKillSwitchEnabled: false,
+        },
+      ],
+    );
+    const optionsStatus = Object.fromEntries(entries) as OptionsStatus;
+
+    const forceDisabledFeatures = options._forceDisabledFeatures;
+    if (Array.isArray(forceDisabledFeatures)) {
+      this.addKillSwitchInformation(forceDisabledFeatures, optionsStatus);
+    }
+
+    return new OptionsConfiguration(optionsStatus);
+  }
+
+  private addKillSwitchInformation(
+    forceDisabledFeatures: OptionCodename[],
+    optionsStatus: OptionsStatus,
+  ) {
+    for (const forceDisabledFeature of forceDisabledFeatures) {
+      if (Object.hasOwn(optionsStatus, forceDisabledFeature)) {
+        optionsStatus[forceDisabledFeature].isKillSwitchEnabled = true;
+      }
+    }
   }
 
   private async notifyListenersIfApplicable(

src/services/options/OptionsProvider.ts
@@ -6,18 +6,32 @@ import {
 
 export interface OptionsProviderPort {
   /**
-   * Returns the value of option |option|.
+   * Returns the raw value of option |option| as configured by the user, that
+   * is, **without** taking into account kill switches.
    */
   getOptionValue<O extends OptionCodename>(
     option: O,
   ): Promise<OptionsValues[O]>;
 
   /**
-   * Returns whether |feature| is enabled.
+   * Returns whether |feature| is enabled, taking into account any enabled kill
+   * switches.
    */
   isEnabled(option: OptionCodename): Promise<boolean>;
 
+  /**
+   * Get options configuration object.
+   */
   getOptionsConfiguration(): Promise<OptionsConfiguration>;
+
+  /**
+   * Get option values, taking into account any enabled kill switches.
+   * @deprecated - Use the options configuration object methods or the options
+   * provider methods. This method has been deprecated because
+   * OptionsConfiguration will/has been changed to not use OptionsValues, since
+   * we also need to store additional information such as if a kill switch is
+   * enabled.
+   */
   getOptionsValues(): Promise<OptionsValues>;
 
   /**

commit 78f3c3967981214647dc114424a50aa2b9ed7856

fix(options-provider): make isEnabled work again with internal options
When calling |isEnabled()| with a boolean internal option, the response
would always be |false| due to a regression caused in
82229aef17df05becc275796d5a97497a9747805.

In that commit, an additional check was added so the method would always
return |false| for options which didn't have |killSwitchType| set to
|KillSwitchType.Option| in the options prototype. This was a check
introduced because we believed we only used that method for feature
options, but that is not the case and thus caused this bug. We now allow
other boolean options to use that method as well.

To improve type safety, we should mark in the options prototype the type
of each option (boolean, string, ...) and only allow calls to this
method for boolean options.

Bug: twpowertools:247
Change-Id: I6a6a696469db2136da02166320e8656ac50800c9

src/common/options/OptionsConfiguration.ts
@@ -62,14 +62,9 @@ export class OptionsConfiguration {
    * Returns whether a feature option is enabled, taking into account any
    * enabled kill switch.
    *
-   * NOTE: the only admitted options are feature options (those marked with
-   * KillSwitchType.Option).
+   * NOTE: the only admitted options are boolean options.
    */
   isEnabled(option: OptionCodename): boolean {
-    if (optionsMap.get(option).killSwitchType !== KillSwitchType.Option) {
-      return false;
-    }
-
     return (
       this.getOptionValue(option) === true && !this.isKillSwitchEnabled(option)
     );

src/common/options/OptionsConfiguration.test.ts
@@ -1,0 +1,46 @@
+import {
+  OptionsConfiguration,
+  OptionsStatus,
+  OptionStatus,
+} from './OptionsConfiguration';
+import { describe, expect, it } from 'vitest';
+import { options, OptionCodename } from './optionsPrototype';
+
+function buildDummyOptionsStatus(
+  override: Partial<OptionsStatus>,
+): OptionsStatus {
+  const defaultOptionsStatus = Object.fromEntries(
+    options.map((option) => {
+      const optionStatus: OptionStatus<OptionCodename> = {
+        value: option.defaultValue,
+        isKillSwitchEnabled: false,
+      };
+      return [option.codename, optionStatus];
+    }),
+  ) as OptionsStatus;
+
+  return {
+    ...defaultOptionsStatus,
+    ...override,
+  };
+}
+
+describe('OptionsConfiguration', () => {
+  describe('isEnabled', () => {
+    // Internal options with boolean values such as |ccdarktheme_switch_status|
+    // should have their value correctly returned by the isEnabled method.
+    it('should return true for an enabled option which is marked as KillSwitchType.Ignore', () => {
+      const optionsStatus = buildDummyOptionsStatus({
+        ccdarktheme_switch_status: {
+          value: true,
+          isKillSwitchEnabled: false,
+        },
+      });
+
+      const sut = new OptionsConfiguration(optionsStatus);
+      const result = sut.isEnabled('ccdarktheme_switch_status');
+
+      expect(result).toBe(true);
+    });
+  });
+});