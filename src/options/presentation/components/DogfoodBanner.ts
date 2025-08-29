40b3f9edad2f612d465b48b1a8cb8ddea61b08fb
Adrià Vilanova Martínez <me@avm99963.com>
2025-08-29 16:41:16 +0200

    feat(options): make dogfood banner localizable
    We now have support in the new options page for languages other than
    English, so this CL makes the dogfood banner localizable for other
    languages as well.
    
    Bug: twpowertools:63
    Change-Id: I6a6a696454f4112a3ca8104312e26de418dddb71

--- src/options/presentation/components/DogfoodBanner.ts
+++ src/options/presentation/components/DogfoodBanner.ts
@@ -1,36 +1,28 @@
-import { html, LitElement } from 'lit';
+import { msg } from '@lit/localize';
+import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { DISCUSS_GROUP_URL } from '../../../common/consts';
+import { I18nLitElement } from '../../../common/litI18nUtils';
 
 import '@material/web/icon/icon.js';
-import { DISCUSS_GROUP_URL } from '../../../common/consts';
 
 const DOGFOOD_ICON =
   'https://www.gstatic.com/images/branding/product/2x/dogfoody_64dp.png';
 
-// NOTE: We will not localize this component unless we want to extend
-// the new options page dogfood to languages other than English.
 @customElement('dogfood-banner')
-export default class DogfoodBanner extends LitElement {
+export default class DogfoodBanner extends I18nLitElement {
   render() {
-    // The full stop after the "discussion group" link shouldn't be
-    // split into a new line. Please remove the following comment while
-    // developing and then readd it to prevent this from happening.
+    const title = msg("You're testing the new options page.");
     // prettier-ignore
+    const description = msg(html`Please leave feedback and report bugs in our <a href=${DISCUSS_GROUP_URL} target="_blank" rel="noreferrer noopener">discussion group</a>. Thanks for testing!`);
+
     return html`
       <banner-card type="tertiary">
         <md-icon slot="icon" aria-hidden="false">
-          <img src=${DOGFOOD_ICON} title="Dogfood" />
+          <img src=${DOGFOOD_ICON} />
         </md-icon>
-        <div slot="title">You're testing the new options page.</div>
-        <div slot="description">
-          Please leave feedback and report bugs in our
-          <a
-            href=${DISCUSS_GROUP_URL}
-            target="_blank"
-            rel="noreferrer noopener"
-          >discussion group</a>.
-          Thanks for testing!
-        </div>
+        <div slot="title">${title}</div>
+        <div slot="description">${description}</div>
       </banner-card>
     `;
   }
