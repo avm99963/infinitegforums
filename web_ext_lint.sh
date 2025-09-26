66f308d15b1497fe7dab4ac019df53b4edbd24a7
Adrià Vilanova Martínez <me@avm99963.com>
2025-09-26 16:12:16 +0200

    test: add web-ext lint test
    This achieves parity with the tests ran in the old
    infinitegforums-legacy-lint pipeline.
    
    Bug: twpowertools:255
    Change-Id: I6a6a6964e0d4fe884ccc4f123dda17891db82529

---
+++ web_ext_lint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v3 ---
+# Copy-pasted from the Bazel Bash runfiles library v3.
+set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+# shellcheck disable=SC1090
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v3 ---
+
+web_ext=$1
+
+if [[ ${2-} == "DO_NOT_RUN" ]]; then
+    echo 2>&1 "Skipping web-ext lint."
+    echo 2>&1 "It only runs when compilation_mode=\"opt\" and browser=\"gecko\"."
+    exit 0
+fi
+
+${web_ext} lint --no-input --source-dir=$(rlocation _main/unpacked_pkg)
