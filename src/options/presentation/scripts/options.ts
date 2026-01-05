02cc6b8dc7afffbfd6476637eb7e98effd4d655b
Adrià Vilanova Martínez <me@avm99963.com>
2026-01-05 17:32:53 +0100

    chore: commit default options when loading the options page
    Bug: twpowertools:261, twpowertools:63
    Change-Id: I167ae7f39d667436baa655792c1e8a676a6a6964

--- src/options/presentation/scripts/options.ts
+++ src/options/presentation/scripts/options.ts
@@ -5,15 +5,33 @@ import '../../ui/components/App';
 import { isProdVersion } from '../../../common/extUtils';
 import { OptionsConfigurationRepositoryAdapter } from '../../infrastructure/repositories/OptionsConfiguration.repository.adapter';
 import { getSyncStorageAreaRepository } from '@/storage/compositionRoot';
+import { DefaultOptionsCommitterAdapter } from '@/options/infrastructure/services/DefaultOptionsCommitter.service.adapter';
 
 function main() {
   const container = document.getElementById('container');
   const app = document.createElement('options-app');
+
   const syncStorageAreaRepository = getSyncStorageAreaRepository();
-  app.optionsProvider = new OptionsProviderAdapter(
+  const optionsProvider = new OptionsProviderAdapter(
     new OptionsConfigurationRepositoryAdapter(syncStorageAreaRepository),
   );
-  app.optionsModifier = new OptionsModifierAdapter(syncStorageAreaRepository);
+  const optionsModifier = new OptionsModifierAdapter(syncStorageAreaRepository);
+
+  // When the user visits the options page, attempt to commit default options.
+  // This is something the old options page did, and we're keeping the same
+  // behavior with the new one.
+  //
+  // The rationale is that if a user visits the options page, we must make sure
+  // that the options the user sees don't change in the future if the default
+  // option changes in |optionsPrototype|.
+  const defaultOptionsCommitter = new DefaultOptionsCommitterAdapter(
+    optionsProvider,
+    optionsModifier,
+  );
+  defaultOptionsCommitter.commit();
+
+  app.optionsProvider = optionsProvider;
+  app.optionsModifier = optionsModifier;
   app.getFeatureCategories = getFeatureCategories;
   app.isProdVersion = isProdVersion();
   container.append(app);
