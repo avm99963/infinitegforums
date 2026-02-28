3b91b89306ebd5c320cd4c2e9b0d6ee2f35d7106
Adrià Vilanova Martínez <me@avm99963.com>
2026-02-28 23:01:05 +0100

    refactor: make release_utils.sh posix-compatible
    This allows it to run correctly in macOS (and maybe also other OSs).
    
    Change-Id: I96fd6d398bd78102aa3fef6292c66cb56a6a6964

--- tools/release_utils.sh
+++ tools/release_utils.sh
@@ -1,14 +1,14 @@
 # Generates the RAW_GIT_VERSION var from Bazel's stable status file.
-function generate_raw_git_version_var() {
+generate_raw_git_version_var() {
   RAW_GIT_VERSION=
   if [ -n "${BAZEL_STABLE_STATUS_FILE-}" ]; then
-    RAW_GIT_VERSION=$(sed --sandbox -n '0,/^STABLE_RAW_GIT_VERSION /{s/^STABLE_RAW_GIT_VERSION \(.*\)$/\1/p}' "${BAZEL_STABLE_STATUS_FILE}")
+    RAW_GIT_VERSION=$(sed -n '/^STABLE_RAW_GIT_VERSION /{s/^STABLE_RAW_GIT_VERSION \(.*\)$/\1/p;q;}' "${BAZEL_STABLE_STATUS_FILE}")
   fi
 }
 
 # Generates the VERSION and VERSION_NAME vars according to
 # RAW_GIT_VERSION.
-function generate_version_vars() {
+generate_version_vars() {
   if [ -z "${RAW_GIT_VERSION-}" ]; then
     echo >&2 "ERROR: RAW_GIT_VERSION is not set or empty."
     return 1
